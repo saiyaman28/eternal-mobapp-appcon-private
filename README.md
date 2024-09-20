5c727f6046d78c9df33a216e7d2eac5a2a433f8a
grt_saiyaman <bayashi.x28@gmail.com>
2024-09-20 22:30:48 +0800

    WEEK 3

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 This app uses AI to monitor energy consumption, detect appliance defects, and provide insights to manage bills. Users can control appliances remotely, set timers, and receive SMS alerts for malfunctions, helping them optimize electricity usage efficiently.
 
 ## Progress
-Added the 3 Splash-Screens & Login Page
+Added the Signup & Forget Pass Pages
--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,39 @@ Color primarycolor = Color.fromRGBO(246, 174, 45, 1.0);
 Color secondarycolor = Color.fromRGBO(34, 71, 73, 1.0);
 Color white = Colors.white;
 
+// HEADER
+double appbarheight = 100;
+
+AppBar header({required BuildContext context, bool automaticallyImplyLeading = false, String pagetitle = ''}) {
+  return AppBar(
+    backgroundColor: Colors.transparent,
+    toolbarHeight: 90,
+    automaticallyImplyLeading: automaticallyImplyLeading,
+    leading: automaticallyImplyLeading ? Container(
+      padding: EdgeInsets.fromLTRB(13, 18, 0, 0),
+      child: IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        iconSize: 40, // Increase the icon size
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    ) : null,
+    centerTitle: true,
+    title: Container(
+        padding: EdgeInsets.only(top: 18),
+        child: Text(pagetitle,
+          style: TextStyle(
+              fontSize: 33,
+              fontWeight: FontWeight.w800,
+              letterSpacing: 1.3,
+              height: 1.2
+          ),
+        )
+    ),
+  );
+}
+
 
 // STARTING PAGE
 class Index extends StatelessWidget {
@@ -586,7 +619,7 @@ class LoginState extends State<Login> {
                                     onTap: (){
                                       Navigator.push(
                                         context,
-                                        MaterialPageRoute(builder: (context) => Login()),
+                                        MaterialPageRoute(builder: (context) => ForgetPass()),
                                       );
                                     },
                                     child: Text(
@@ -609,7 +642,7 @@ class LoginState extends State<Login> {
                                         onTap: (){
                                           Navigator.push(
                                             context,
-                                            MaterialPageRoute(builder: (context) => Login()),
+                                            MaterialPageRoute(builder: (context) => SignUp()),
                                           );
                                         },
                                         child: Text(
@@ -632,6 +665,328 @@ class LoginState extends State<Login> {
                   ),
                 ),
               ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+class SignUp extends StatefulWidget {
+  SignUp({super.key});
+  @override
+  SignUpState createState() => SignUpState();
+}
+
+class SignUpState extends State<SignUp> {
+  bool register_isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: bgcolor2,
+        appBar: header(context: context, automaticallyImplyLeading: true),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 700,
+                padding: EdgeInsets.symmetric(horizontal: 23),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Sign Up',
+                                style: TextStyle(
+                                    fontSize: 43,
+                                    fontWeight: FontWeight.w800,
+                                    letterSpacing: 1.3,
+                                    height: 1.2
+                                ),
+                              ),
+                              Text('Create your account here',
+                                  style: TextStyle(
+                                    color: secondarycolor,
+                                    fontSize: 17.23,
+                                    fontWeight: FontWeight.w800,
+                                  )
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 23),
+                        Column(
+                          children: [
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: 'First Name',
+                                filled: true,
+                                fillColor: white,
+                                contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 28),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 18),
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Last Name',
+                                filled: true,
+                                fillColor: white,
+                                contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 28),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 18),
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Email',
+                                filled: true,
+                                fillColor: white,
+                                contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 28),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 18),
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Home Address',
+                                filled: true,
+                                fillColor: white,
+                                contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 28),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 18),
+                            TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: 'Password',
+                                filled: true,
+                                fillColor: white,
+                                contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 28),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 18),
+                            TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: 'Confirm Password',
+                                filled: true,
+                                fillColor: white,
+                                contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 28),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Theme(
+                        data: Theme.of(context).copyWith(
+                          splashColor: Colors.transparent,
+                          highlightColor: Colors.transparent,
+                          checkboxTheme: CheckboxThemeData(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                              side: BorderSide(
+                                  color: Colors.transparent
+                              ),
+                              fillColor: MaterialStateProperty.all(white)
+                          ),
+                        ),
+                        child: CheckboxListTile(
+                          title: Text('I agree to the terms and conditions',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                height: 1
+                            ),
+                          ),
+                          value: register_isChecked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              register_isChecked = value ?? false;  // Update the state
+                            });
+                          },
+                          controlAffinity: ListTileControlAffinity.leading,
+                          activeColor: primarycolor,
+                          contentPadding: EdgeInsets.zero,
+                        )
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: primarycolor,
+                            foregroundColor: white,
+                            fixedSize: Size.fromWidth(MediaQuery.of(context).size.width),
+                            padding: EdgeInsets.symmetric(vertical: 13),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(13)
+                            )
+                        ),
+                        child: Text('Submit',
+                            style: TextStyle(
+                                fontSize: 23,
+                                fontWeight: FontWeight.w400
+                            )
+                        )
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class ForgetPass extends StatefulWidget {
+  ForgetPass({super.key});
+  @override
+  ForgetPassState createState() => ForgetPassState();
+}
+
+class ForgetPassState extends State<ForgetPass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: bgcolor2,
+        appBar: header(context: context, automaticallyImplyLeading: true),
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height - appbarheight - (appbarheight/7),
+            padding: EdgeInsets.symmetric(horizontal: 23),
+            child: Center(
+              child: Container(
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Forget Password',
+                                style: TextStyle(
+                                    fontSize: 43,
+                                    fontWeight: FontWeight.w800,
+                                    letterSpacing: 1.3,
+                                    height: 1.2
+                                ),
+                              ),
+                              Text('Don’t worry about your account',
+                                  style: TextStyle(
+                                    color: secondarycolor,
+                                    fontSize: 17.23,
+                                    fontWeight: FontWeight.w800,
+                                  )
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 23),
+                        TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            filled: true,
+                            fillColor: white,
+                            contentPadding: EdgeInsets.symmetric(vertical: 18, horizontal: 28),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide.none,
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Login()),
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                            backgroundColor: primarycolor,
+                            foregroundColor: white,
+                            fixedSize: Size.fromWidth(MediaQuery.of(context).size.width),
+                            padding: EdgeInsets.symmetric(vertical: 13),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(13)
+                            )
+                        ),
+                        child: Text('Submit',
+                            style: TextStyle(
+                                fontSize: 23,
+                                fontWeight: FontWeight.w400
+                            )
+                        )
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         )
